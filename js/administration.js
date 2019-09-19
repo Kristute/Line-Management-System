c318e41b20271761dea7c76a7489b523e94df02a
Kristina <R.Kryste@gmail.com>
2019-09-20 00:39:08 +0300

    Added data upload to local storage, added specialists dropdown initialization

--- js/administration.js
+++ js/administration.js
@@ -1,3 +1,50 @@
-function importData() {
-    console.log('Save to localstorage');
+$('#load-data').click(() => {
+  $.when(
+    loadJsonDataToLocalStorage("./data/service.json", "line-data"),
+    loadJsonDataToLocalStorage("./data/clients.json", "line-clients"),
+    loadJsonDataToLocalStorage("./data/specialists.json", "line-specialists")
+  ).then(
+    () => {
+      $('#import-notification-success').removeClass('d-none');
+      $('#import-notification-success').addClass('show');
+      initializeSpecialistsSelector();  
+    },
+    () => {
+      $('#import-notification-error').removeClass('d-none');
+      $('#import-notification-error').addClass('show');
+    }
+  );
+});
+
+function loadJsonDataToLocalStorage(url, localStorageKey) {
+  return $.ajax({
+    type: "Get",
+    url: url,
+    dataType: "json",
+    success: (data) => {
+      localStorage.setItem(localStorageKey, JSON.stringify(data));
+    },
+    error: (e) => {
+        console.log(e);
+    }
+});
+}
+
+function initializeSpecialistsSelector () {
+  const specialistsData = localStorage.getItem('line-specialists');
+  const dropdown = $('#specialists-dropdown')
+
+  if (specialistsData !== null) {
+    dropdown.empty();
+    dropdown.append('<option selected="true" disabled>Choose Specialist</option>');
+    dropdown.prop('selectedIndex', 0);
+
+    $.each(JSON.parse(specialistsData), (key, entry) => {
+      dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.specialist));
+    })
+
+    dropdown.removeClass('d-none');
   }
+}
+
+initializeSpecialistsSelector();
