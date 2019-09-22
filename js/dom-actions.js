b78296142705c12b0ecfd15c134af806c595d8b2
Kristina <R.Kryste@gmail.com>
2019-09-23 01:12:10 +0300

    Added button "Serviced" to specialist page and binded event

--- js/dom-actions.js
+++ js/dom-actions.js
@@ -1,7 +1,6 @@
 export function initializeSpecialistsSelector() {
   const specialistsData = localStorage.getItem('line-specialists');
   const dropdown = $('#specialists-dropdown');
-
   if (specialistsData !== null) {
     $.each(JSON.parse(specialistsData), (key, entry) => {
       dropdown.append($(`<span class="dropdown-item js-specialists-dropdown cursor-pointer" data-value=${entry.ID}>${entry.specialist}</span>`));
@@ -11,6 +10,8 @@ export function initializeSpecialistsSelector() {
   }
 }
 
-export function generateTbodyRow(client, active) {
-  return `<tr><td ${active ? 'class="bg-success"' : ''}>${client.number}</td></tr>`;
+export function generateTbodyRow(client, active, buttonEnabled = false) {
+  const clientNumberColumn = `<td ${active ? 'class="bg-success"' : ''}>${client.number}</td>`;
+  const buttonColumn = buttonEnabled ? `<td><button type="button" class="btn btn-success w-25 p-3 js-servised" data-value=${client.ID}>Serviced</button></td>` : '';
+  return `<tr>${clientNumberColumn}${buttonColumn}</tr>`;
 }
--- js/specialist.js
+++ js/specialist.js
@@ -1,11 +1,18 @@
 import { initializeSpecialistsSelector, generateTbodyRow } from './dom-actions';
-import { getClientsBySpecialist } from './storage-management';
+import { getClientsBySpecialist, markAsServiced } from './storage-management';
 
 initializeSpecialistsSelector();
 
 function renderClientsList(specialistName, clients) {
   $('.js-thead').html(`<tr><th>${specialistName}</th><tr>`);
-  $('.js-tbody').html(clients.sort((a, b) => a.number - b.number).map((client, index) => generateTbodyRow(client, index === 0)).join(''));
+  $('.js-tbody').html(clients.sort((a, b) => a.number - b.number).map((client, index) => generateTbodyRow(client, index === 0, true)).join(''));
+}
+
+function bindOnClickEvents() {
+  $('.js-servised').click((event) => {
+    const clientID = parseInt(event.currentTarget.dataset.value, 10);
+    markAsServiced(clientID);
+  });
 }
 
 $('.js-specialists-dropdown').click((event) => {
@@ -13,4 +20,5 @@ $('.js-specialists-dropdown').click((event) => {
   const specialistName = $(event.currentTarget).html();
   const clientsBySpecialist = getClientsBySpecialist(specialistID);
   renderClientsList(specialistName, clientsBySpecialist);
+  bindOnClickEvents();
 });
